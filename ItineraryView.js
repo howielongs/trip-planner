dfab61cc445eb86995d71e5e90bfd85a114b1140
Luo Long <97410344+howielongs@users.noreply.github.com>
2024-10-29 11:45:26 -0700

    updated itinerary design

---
+++ ItineraryView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Itinerary from '../components/Itinerary';
+
+const ItineraryView = () => {
+  return (
+    <div className="itinerary-view">
+      <Itinerary />
+    </div>
+  );
+};
+
+export default ItineraryView;
